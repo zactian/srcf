0d89f1daf91beee4e0f741678848c5dc588a9dfe
Zach Strenfel <zwstrenfel@gmail.com>
2015-04-24 13:30:27 -0700

    improved pdf table, style still unfinished

--- javascript/form.js
+++ javascript/form.js
@@ -4,16 +4,7 @@ $(document).ready(function() {
 	 * if so, unfocusing the confirm email textbox will not show another
 	 * error message until the initial email is validated */
 	var emailInvalid = true;
-
-	//not needed anymore -- delete later
-	// /* Maps school to their pdf instructions */
-	// var schoolToPDF = {"chemistry": "https://mysummer-dev-demo.berkeley.edu/srcf/serveCollegePdf.php?id=2",
-	// 				 "engineering": "https://mysummer-dev-demo.berkeley.edu/srcf/serveCollegePdf.php?id=3",
-	// 				 "enviro-design": "https://mysummer-dev-demo.berkeley.edu/srcf/serveCollegePdf.php?id=4",
-	// 				 "business": "https://mysummer-dev-demo.berkeley.edu/srcf/serveCollegePdf.php?id=6",
-	// 				 "l-and-s": "https://mysummer-dev-demo.berkeley.edu/srcf/serveCollegePdf.php?id=1",
-	// 				 "natural-resources": "https://mysummer-dev-demo.berkeley.edu/srcf/serveCollegePdf.php?id=1",
-	// 				 "law": "https://mysummer-dev-demo.berkeley.edu/srcf/serveCollegePdf.php?id=7"};
+	var pdfsDisplayed = 0;
 
 	/** warns the email if their email is not formmatted correctly */
 	$("#email-init").blur(function() {
@@ -49,48 +40,29 @@ $(document).ready(function() {
 
 
 	/*Displays PDF instructions after School(s) is/are choosen */
-	$("input[type=checkbox]").change( function() {
+	$("input[type=checkbox]").change(function() {
 		var college = $(this).val();
 
-		$("#pdfs").slideDown("slow");
-
 	    if($(this).is(":checked")){
-	    	$('<td id = "' + college + '"><img src="https://api.fnkr.net/testimg/50x70/00CED1/FFF/?text=img+placeholder"> <br>' +
-                     college + '</td>').hide().appendTo("#pdf-table tr:last").fadeIn(1500);
-	       // $("#pdf-table tr:last").append( '<td><img src="https://api.fnkr.net/testimg/50x70/00CED1/FFF/?text=img+placeholder"> <br>' +
-        //              $(this).val() + '</td>').fadeIn(999);
-	       // $
+	    	$('<td id="' + college + '"><a href="" target = "_blank" data-college ="' + college +
+	    		'"><img src="https://api.fnkr.net/testimg/50x70/00CED1/FFF/?text=img+placeholder"></a> <br>' +
+                 college + '</td>').hide().appendTo("#pdf-table tr:last").fadeIn(1500);
+	    	pdfsDisplayed ++;
 	    } else {
 	    	$('#' + college).fadeOut("slow", function() {
 	    		$(this).remove()
 	    	});
+	    	pdfsDisplayed --;
 	    }
 
+		if (pdfsDisplayed > 0) {
+			$("#pdfs").slideDown("slow");
+		} else {
+			$("#pdfs").slideUp("slow");
+		}
 	  });
 
     $("#example2").tableSorter();
-
-
-
-
-	// $(":radio").click(function() {
-	// 	var confirmed = false;
-	// 	$(":text").each(function() {
-	// 		if ($("this").value != "") {
-	// 			confirmed = true;
-	// 		} else {
-	// 			confirmed = false;
-	// 		}
-	// 	})
-	// })
-
-
-	// $(":input").each(function() {
-	// 	console.log($(this).val());
-	//    if($(this).val() === "") {
-	//    	$("submit-button").addClass("disabled");
-	//    }
-	// });
 });
 
 function tableSorter(table,  sortFns){
