3891c7c04a7932e6346b006e6a1abe9a8689dccf
Zach Strenfel <zwstrenfel@gmail.com>
2015-04-24 12:27:10 -0700

    added pdf functionality to form page

--- javascript/form.js
+++ javascript/form.js
@@ -5,6 +5,16 @@ $(document).ready(function() {
 	 * error message until the initial email is validated */
 	var emailInvalid = true;
 
+	//not needed anymore -- delete later
+	// /* Maps school to their pdf instructions */
+	// var schoolToPDF = {"chemistry": "https://mysummer-dev-demo.berkeley.edu/srcf/serveCollegePdf.php?id=2",
+	// 				 "engineering": "https://mysummer-dev-demo.berkeley.edu/srcf/serveCollegePdf.php?id=3",
+	// 				 "enviro-design": "https://mysummer-dev-demo.berkeley.edu/srcf/serveCollegePdf.php?id=4",
+	// 				 "business": "https://mysummer-dev-demo.berkeley.edu/srcf/serveCollegePdf.php?id=6",
+	// 				 "l-and-s": "https://mysummer-dev-demo.berkeley.edu/srcf/serveCollegePdf.php?id=1",
+	// 				 "natural-resources": "https://mysummer-dev-demo.berkeley.edu/srcf/serveCollegePdf.php?id=1",
+	// 				 "law": "https://mysummer-dev-demo.berkeley.edu/srcf/serveCollegePdf.php?id=7"};
+
 	/** warns the email if their email is not formmatted correctly */
 	$("#email-init").blur(function() {
 		var email = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
@@ -37,22 +47,24 @@ $(document).ready(function() {
 		}
 	});
 
-	// $(":radio").click(function() {
-	// 	var confirmed = false;
-	// 	$(":text").each(function() {
-	// 		if ($("this").value != "") {
-	// 			confirmed = true;
-	// 		} else {
-	// 			confirmed = false;
-	// 		}
-	// 	})
-	// })
+	/*Displays PDF instructions after School(s) is/are choosen */
+	$("input[type=checkbox]").change( function() {
+		var college = $(this).val();
 
+		$("#pdfs").slideDown("slow");
 
-	// $(":input").each(function() {
-	// 	console.log($(this).val());
-	//    if($(this).val() === "") {
-	//    	$("submit-button").addClass("disabled");
-	//    }
-	// });
+	    if($(this).is(":checked")){
+	    	$('<td id = "' + college + '"><img src="https://api.fnkr.net/testimg/50x70/00CED1/FFF/?text=img+placeholder"> <br>' +
+                     college + '</td>').hide().appendTo("#pdf-table tr:last").fadeIn(1500);
+	       // $("#pdf-table tr:last").append( '<td><img src="https://api.fnkr.net/testimg/50x70/00CED1/FFF/?text=img+placeholder"> <br>' +
+        //              $(this).val() + '</td>').fadeIn(999);
+	       // $
+	    } else {
+	    	$('#' + college).fadeOut("slow", function() {
+	    		$(this).remove()
+	    	});
+	    }
+
+	  });
+
 })
