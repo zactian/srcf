2824405bbf1013549dc946f2f97feb21708ebc3d
Zach Strenfel <zwstrenfel@gmail.com>
2015-05-08 14:01:36 -0700

    work on action page

--- javascript/action.js
+++ javascript/action.js
@@ -1,12 +1,41 @@
 $(document).ready(function() {
 
-	$(".accordian h3").click(function(){
+	$(".accordian .item-header").click(function(){
 			console.log("click");
+			$('.accordian img').removeClass("rotated");
 			//slide up all the link lists
 			$(".accordian .insert").slideUp();
 			//slide down the link list below the h3 clicked - only if its closed
 			if(!$(this).parent().next('.insert').is(":visible")){
 				$(this).parent().next('.insert').slideDown();
+				$(this).children('img').addClass("rotated");
 			}
 		});
+	//setup popups
+	 $('#my_popup').popup();
+
+
+	$( "#department_id" ).change(function() {
+	    $("#course_id").show();
+	});
+
+	$( "#course_id" ).change(function() {
+	    $("#section_id").show();
+	});
+
+
+	$( "#section_id" ).change(function() {
+		$("#course-example").fadeIn("slow");
+	});
+
+	$("#ccn").keyup(function() {
+	    if($(this).val().length == 5){
+	        $("#ccn_info").fadeIn(1000);
+	        $(".import-container").delay(700).fadeIn(1000);
+	    }
+	    // else{
+	    //     $(".ccn_info").hide();
+	    //     $(".import-container").hide();
+	    // }
+	});
 });
