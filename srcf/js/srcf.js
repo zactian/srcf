75d60f4d5210690532f61dd9ba2960f696c0c959
timcheng <timcheng1022@gmail.com>
2015-06-05 16:58:59 -0700

    test

---
+++ srcf/js/srcf.js
@@ -0,0 +1,25 @@
+(function ($) {
+
+    $.fn.Append= function (options) {
+
+        var settings = $.extend({}, options);
+        var obj = this;
+
+        var body = $("#college_pdfs");
+
+        var ray = (function () {
+            var that = this;
+            that.main = {
+                firstAction: function () {
+                },
+                secondAction: function () {
+                },
+                finalAction: function () {
+                }
+            }
+            that.main.firstAction();
+        })();
+    };
+
+
+}(jQuery));
